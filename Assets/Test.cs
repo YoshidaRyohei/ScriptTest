3e0f993302995f53b5ec875c18ac89c2da22f9b9
吉田燎平 <y.ryohei.1204@gmail.com>
2017-10-31 21:03:57 +0900

    ScriptTest 課題提出
    発展版の課題を制作しました。

---
+++ Assets/Test.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 魔法使いクラス
+/// </summary>
+public class Magician
+{
+	#region プロパティ
+	/// <summary>
+	/// MP
+	/// </summary>
+	private int m_MP = 53;
+	#endregion
+
+	/// <summary>
+	/// 魔法を使用する
+	/// </summary>
+	/// <returns>使用できたかどうか</returns>
+	public bool Magic()
+	{
+		if (m_MP >= 5)
+		{
+			m_MP -= 5;
+			Debug.Log("魔法攻撃をした。残りMPは" + m_MP + "。");
+			return true;
+		}
+		else
+		{
+			Debug.Log("MPが足りないため魔法が使えない。");
+			return false;
+		}
+	}
+}
+
+/// <summary>
+/// テストクラス
+/// </summary>
+public class Test : MonoBehaviour {
+	private Magician m_Magician;
+
+	void Start ()
+	{
+		m_Magician = new Magician();
+		while (true)
+		{
+			if (!m_Magician.Magic())
+				break;
+		}
+	}
+	
+	void Update ()
+	{
+		
+	}
+}
